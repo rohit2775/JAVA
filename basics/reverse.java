23156e83c1c09c8f9a58896976bad6771c339a58
rohit2775 <rg7558674@gmail.com>
2026-06-25 11:45:48 +0530

    loops going

---
+++ basics/reverse.java
@@ -0,0 +1,21 @@
+
+package basics;
+ 
+public class reverse {
+
+    public static void main(){
+
+      int n = 987643;
+
+      while (n>0){
+        int reverse = n%10;
+        System.out.print(reverse);
+        n/=10;
+
+
+      }
+
+    }
+}
+
+// bss logic yaad rakhna hai 
---
+++ basics/square.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class square {
+
+    public static void main (){
+       
+        for(int i=0;i<=3;i++){
+            System.err.println("* * *  *");
+        }
+
+    }
+}
