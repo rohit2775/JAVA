20fd26f32a8b30d2cdd4c39689b51998a1b27cbf
rohit2775 <rg7558674@gmail.com>
2026-06-26 11:32:40 +0530

    loops done

--- basicpatterns/characterpattern.java
+++ basicpatterns/characterpattern.java
@@ -4,7 +4,28 @@ public class characterpattern {
     
     public static void main (){
 
+        int n = 5;
+        char ch = 'A';
 
+
+     for(int i =1 ; i<=n;i++){
+               
+         for(int chars=1 ; chars<=i ; chars++){
+
+            System.out.print(ch + " ");
+            ch++;
+
+         }
+         System.out.println();
+
+     }
         
     }
 }
+
+
+// A 
+// B C 
+// D E F 
+// G H I J 
+// K L M N O 
